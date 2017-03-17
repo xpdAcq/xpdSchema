18bd70e3cd799d64d46bd1141ace036f470cf5e1
christopher <cjwright4242@gmail.com>
2017-03-17 14:44:15 -0400

    ENH: add schemas to __init__

---
+++ xpdschema/__init__.py
@@ -0,0 +1,18 @@
+import json
+from enum import Enum
+from pkg_resources import resource_filename as rs_fn
+
+
+__all__ = ['DocumentNames', 'schemas']
+
+
+class DocumentNames(Enum):
+    start = 'start'
+
+
+SCHEMA_PATH = 'schemas'
+SCHEMA_NAMES = {DocumentNames.start: 'schemas/run_start.json'}
+schemas = {}
+for name, filename in SCHEMA_NAMES.items():
+    with open(rs_fn('schemas', filename)) as fin:
+        schemas[name] = json.load(fin)
