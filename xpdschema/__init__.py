9d48ecc790def8945bdcaa956f49a4692c2017ce
christopher <cjwright4242@gmail.com>
2017-03-17 16:49:07 -0400

    ENH: remove document enum

--- xpdschema/__init__.py
+++ xpdschema/__init__.py
@@ -1,17 +1,12 @@
 import json
-from enum import Enum
 from pkg_resources import resource_filename as rs_fn
 
 
 __all__ = ['DocumentNames', 'schemas']
 
 
-class DocumentNames(Enum):
-    start = 'start'
-
-
 SCHEMA_PATH = 'xpdschema'
-SCHEMA_NAMES = {DocumentNames.start: 'schemas/run_start.json'}
+SCHEMA_NAMES = {'start': 'schemas/run_start.json'}
 schemas = {}
 for name, filename in SCHEMA_NAMES.items():
     with open(rs_fn(SCHEMA_PATH, filename)) as fin:
